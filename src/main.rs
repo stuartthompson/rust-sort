30c3378fd45a6490c848f0a9bece204ba00a7e95
Stuart Thompson <stuart@stuartthompson.net>
2022-08-26 18:03:09 -0700

    Adds a new run case to the main function.

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,15 @@ use radix_sort::radix_sort;
 fn main() {
     // Counting sort
     let list = vec![4,5,1,7,6,5];
-    let sorted = counting_sort(&list, 1);
-    
-    println!("Counting sort {:?} => {:?}", list, sorted);
+    println!("Counting sort {:?} => {:?}", list, counting_sort(&list, 1));
 
     // Bubble sort
-    let list = vec![5,1,7,3,4,3];
-    let sorted = bubble_sort(&list);
+    let list = vec![45, 31, 64, 72, 46, 5, 1];
+    println!("  Bubble sort {:?} => {:?}", list, bubble_sort(&list));
 
-    println!("  Bubble sort {:?} => {:?}", list, sorted);
-
+    // Radix sort
     let list = vec![1938, 1921, 1985, 1964];
-    let sorted = radix_sort(&list);
-
-    println!("   Radix sort {:?} => {:?}", list, sorted);
+    println!("   Radix sort1 {:?} => {:?}", list,  radix_sort(&list));
+    let list = vec![1934, 651, 77712, 19, 173, 31];
+    println!("   Radix sort2 {:?} => {:?}", list, radix_sort(&list));
 }
