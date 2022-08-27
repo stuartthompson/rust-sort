8aaa90a6998e33e766122298455c91cba8ad2d7e
Stuart Thompson <stuart@stuartthompson.net>
2022-08-26 17:59:03 -0700

    Updates the main method to use the counting and radix sorts.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod bubble_sort;
 mod counting_sort;
+mod radix_sort;
 use bubble_sort::bubble_sort;
 use counting_sort::counting_sort;
+use radix_sort::radix_sort;
 
 fn main() {
     // Counting sort
     let list = vec![4,5,1,7,6,5];
-    let sorted = counting_sort(&list);
-
+    let sorted = counting_sort(&list, 1);
+    
     println!("Counting sort {:?} => {:?}", list, sorted);
 
     // Bubble sort
@@ -15,4 +17,9 @@ fn main() {
     let sorted = bubble_sort(&list);
 
     println!("  Bubble sort {:?} => {:?}", list, sorted);
+
+    let list = vec![1938, 1921, 1985, 1964];
+    let sorted = radix_sort(&list);
+
+    println!("   Radix sort {:?} => {:?}", list, sorted);
 }
